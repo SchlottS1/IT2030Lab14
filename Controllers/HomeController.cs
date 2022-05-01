93539fa94f5655007d41c33d9311715f2ce25379
Spencer <spencerschlott@yahoo.com>
2022-04-30 22:53:08 -0400

    Initial check-in of module ClassSchedule

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ClassSchedule.Models;
+
+namespace ClassSchedule.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHttpContextAccessor http { get; set; }
+        private IClassScheduleUnitOfWork data { get; set; }
+
+        public HomeController(IClassScheduleUnitOfWork unit, IHttpContextAccessor ctx)
+        {
+            data = unit;
+            http = ctx;
+        }
+
+        public ViewResult Index(int id)
+        {
+            if (id > 0)
+            {
+                http.HttpContext.Session.SetInt32("dayid", id);
+            }
+
+            var dayOptions = new QueryOptions<Day>
+            {
+                OrderBy = d => d.DayId
+            };
+
+            var classOptions = new QueryOptions<Class>
+            {
+                Includes = "Teacher, Day"
+            };
+
+            if (id == 0)
+            {
+                classOptions.OrderBy = c => c.DayId;
+            }
+            else
+            {
+                classOptions.Where = c => c.DayId == id;
+                classOptions.OrderBy = c => c.MilitaryTime;
+            }
+
+            ViewBag.Days = data.Days.List(dayOptions);
+            return View(data.Classes.List(classOptions));
+        }
+    }
+}
